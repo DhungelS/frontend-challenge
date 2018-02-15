9732ad291f001faf2dbba26dbf43b76de3b1f9d2
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-02-15 18:45:23 -0500

    Tests working

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,43 @@ const Mix = (function () {
 
   function render() {
     let viewEvents = Store.events;
-    const currentView = generateEventLists(viewEvents);
+    const eventListView = generateEvents(viewEvents);
 
-    const groups = [];
+    const groupsList = Store.splitArr(eventListView, 4)
 
-    for (let i = 0; i < currentView.length; i += 4) {
-      groups.push(currentView.slice(i, i + 4));
-    }
-    
-    $('.deck-one').html(groups[0]);
-    $('.deck-two').html(groups[1]);
-    $('.deck-three').html(groups[2]);
+    $('.deck-one').html(groupsList[0]);
+    $('.deck-two').html(groupsList[1]);
+    $('.deck-three').html(groupsList[2]);
+
+    const eventInfoView = generateEvents(viewEvents)
+    $('.events-row-one').html(eventInfoView);
+    // const groupsEvents = Store.splitArr(eventInfoView, 4)
+    // console.log(groupsEvents);
+    // $('.events-row-one').html(groupsEvents[0])
+
+
   }
 
   function generateEventList(item) {
-    return (`
+
+    if (Store.eventsPage) {
+
+      return (`
+      <div class="col">
+        <div class="card" style="width: 20rem;">
+          <img class="card-img-top" src="${item.image}" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">I'm Going!</a>
+          </div>
+        </div>
+      </div>
+  `);
+    }
+
+    else {
+      return (`
     <a href = "#">
     <div class="card card-nav-tabs" style="width: 20rem;">
     <div class="card-header card-header-success">
@@ -28,10 +50,18 @@ const Mix = (function () {
     </div>
   </div>
   </a>
-        `);
+      `);
+    }
+
   }
 
-  function generateEventLists(events) {
+  function handleEventsPageClicked() {
+    $('.events-page').on('click', function () {
+      Store.eventsPage === true;
+    });
+  }
+
+  function generateEvents(events) {
     return events.map(event => {
       return generateEventList(event);
     });
@@ -39,7 +69,7 @@ const Mix = (function () {
   }
 
   function bindEventListeners() {
-
+    handleEventsPageClicked();
 
   }
 
--- scripts/store.js
+++ scripts/store.js
@@ -1,8 +1,18 @@
 'use strict';
 const Store = (function () {
 
+  const splitArr = ((arr, chunkSize) => {
+    var groups = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      groups.push(arr.slice(i, i + chunkSize));
+    }
+    return groups;
+  });
+
   return {
-    events: []
+    events: [],
+    splitArr,
+    eventsPage: false
   };
 
 }());
