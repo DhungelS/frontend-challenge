14e2fe9e94b5f8baf42117a9d39fa2e9618f1b78
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-02-17 12:17:29 -0500

    changes to enable https requests

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,7 @@
 
 const Api = (function () {
 
-  const BASE_URL = 'http://www.mocky.io/v2/5a8505c73000002a0069b0c0';
+  const BASE_URL = 'https://www.mocky.io/v2/5a8505c73000002a0069b0c0';
 
   const getItems = function () {
     return $.ajax({
